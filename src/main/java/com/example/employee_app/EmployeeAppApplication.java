e22204cca058b94b76efaf8a6a9818c8a12a38f0
mary <melina.m2011@gmail.com>
2022-08-10 22:06:08 +0300

    modify some classes to solve deploy issue

--- src/main/java/com/example/employee_app/EmployeeAppApplication.java
+++ src/main/java/com/example/employee_app/EmployeeAppApplication.java
@@ -2,12 +2,17 @@ package com.example.employee_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeAppApplication {
-
+public class EmployeeAppApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeAppApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmployeeAppApplication.class);
+    }
 }
